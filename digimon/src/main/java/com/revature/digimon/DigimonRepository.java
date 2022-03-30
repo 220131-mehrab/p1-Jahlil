feb617cdfdb2f93d3806a6893e41acc5ce5df1a0
xxxjahlilallenxxx <jahlilallen@gmail.com>
2022-03-30 18:01:06 -0400

    Updated 1 file and Created 1 file

--- digimon/src/main/java/com/revature/digimon/DigimonRepository.java
+++ digimon/src/main/java/com/revature/digimon/DigimonRepository.java
@@ -1,7 +1,9 @@
 package com.revature.digimon;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface DigimonRepository extends JpaRepository<Digimon, Long> {
     
 }
